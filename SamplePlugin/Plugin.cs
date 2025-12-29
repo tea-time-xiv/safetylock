dac1f34a496cbc6136a0dcd39421301cc736ee81
Tea Time <tea-time-13371235@proton.me>
2025-12-29 19:23:31 +0100

    Remove unused plugin services to streamline code and improve maintainability

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -12,11 +12,7 @@ namespace SamplePlugin;
 public sealed class Plugin : IDalamudPlugin
 {
     [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
-    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
-    [PluginService] internal static IPlayerState PlayerState { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
     [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
     [PluginService] internal static IAddonLifecycle AddonLifecycle { get; private set; } = null!;
